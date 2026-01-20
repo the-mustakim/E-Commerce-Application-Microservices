b52fb20f9687f34ac5cb73786d3d48761b1890b4
Mustakim <mustakimsayyed50@gmail.com>
2026-01-20 23:09:38 +0000

    Externalize the configurations to config-server and .env files

---
+++ ecom-config-server/src/main/java/com/ecommerce/ecom_config_server/EcomConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecom_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class EcomConfigServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EcomConfigServerApplication.class, args);
+	}
+}
---
+++ ecom-config-server/src/test/java/com/ecommerce/ecom_config_server/EcomConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecom_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
