166649e39f4acf86faa4c10cc5901c4ba0cc0cd7
Mustakim <mustakimsayyed50@gmail.com>
2026-05-31 16:46:23 +0100

    Section 17: Observability in Spring Boot Microservices: Logs, Metrics & Tracing

--- order/src/main/java/com/ecommerce/order/clients/ProductServiceConfig.java
+++ order/src/main/java/com/ecommerce/order/clients/ProductServiceConfig.java
@@ -1,29 +1,89 @@
 package com.ecommerce.order.clients;
 
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-
+import org.springframework.http.HttpHeaders;
 import java.util.Optional;
 
 @Configuration
 public class ProductServiceConfig {
 
+    @Autowired(required = false)
+    private Tracer tracer;
+
+    @Autowired(required = false)
+    private Propagator propagator;
+
+
     @Bean
     public ProductServiceClient productServiceClient(LoadBalancerClient loadBalancerClient) {
-        RestClient restClient = RestClient.builder()
-                .baseUrl("http://product-service")
-                .defaultStatusHandler(HttpStatusCode::is4xxClientError, ((request, response) -> Optional.empty()))
-                .requestInterceptor(new LoadBalancerInterceptor(loadBalancerClient))
-                .build();
+//        RestClient restClient = RestClient.builder()
+//                .baseUrl("http://product-service")
+//                .defaultStatusHandler(HttpStatusCode::is4xxClientError, ((request, response) -> Optional.empty()))
+//                .requestInterceptor(new LoadBalancerInterceptor(loadBalancerClient))
+//                .requestInterceptor(createTracingInterceptor())
+//                .build();
 
-        RestClientAdapter adapter = RestClientAdapter.create(restClient);
-        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
-        return factory.createClient(ProductServiceClient.class);
+        RestClient.Builder builder = RestClient.builder()
+                .baseUrl("http://product-service")
+                .defaultStatusHandler(
+                        HttpStatusCode::is4xxClientError,
+                        (request, response) -> Optional.empty()
+                )
+                .requestInterceptor(
+                        new LoadBalancerInterceptor(loadBalancerClient)
+                );
+
+        // Add tracing only if tracing dependencies exist
+        if (tracer != null && propagator != null) {
+            builder.requestInterceptor(
+                    createTracingInterceptor()
+            );
+        }
+
+        RestClient restClient = builder.build();
+
+        RestClientAdapter adapter =
+                RestClientAdapter.create(restClient);
+
+        HttpServiceProxyFactory factory =
+                HttpServiceProxyFactory
+                        .builderFor(adapter)
+                        .build();
+
+        return factory.createClient(
+                ProductServiceClient.class
+        );
+    }
+
+    private ClientHttpRequestInterceptor createTracingInterceptor() {
+
+        return (request, body, execution) -> {
+
+            if (tracer.currentSpan() != null) {
+
+                HttpHeaders headers = request.getHeaders();
+
+                propagator.inject(
+                        tracer.currentSpan().context(),
+                        headers,
+                        HttpHeaders::add
+                );
+            }
+            return execution.execute(
+                    request,
+                    body
+            );
+        };
     }
 }
--- order/src/main/java/com/ecommerce/order/clients/UserServiceConfig.java
+++ order/src/main/java/com/ecommerce/order/clients/UserServiceConfig.java
@@ -1,10 +1,15 @@
 package com.ecommerce.order.clients;
 
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -14,17 +19,65 @@ import java.util.Optional;
 @Configuration
 public class UserServiceConfig {
 
+
+    @Autowired(required = false)
+    private Tracer tracer;
+
+    @Autowired(required = false)
+    private Propagator propagator;
+
     @Bean
     public UserServiceClient userServiceClient(LoadBalancerClient loadBalancerClient) {
-        RestClient restClient = RestClient.builder()
-                .baseUrl("http://user-service")
-                .defaultStatusHandler(HttpStatusCode::is4xxClientError, ((request, response) -> Optional.empty()))
-                .requestInterceptor(new LoadBalancerInterceptor(loadBalancerClient))
-                .build();
+//        RestClient restClient = RestClient.builder()
+//                .baseUrl("http://user-service")
+//                .defaultStatusHandler(HttpStatusCode::is4xxClientError, ((request, response) -> Optional.empty()))
+//                .requestInterceptor(new LoadBalancerInterceptor(loadBalancerClient))
+//                .build();
 
-        RestClientAdapter adapter = RestClientAdapter.create(restClient);
-        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+        RestClient.Builder builder = RestClient.builder()
+                .baseUrl("http://user-service")
+                .defaultStatusHandler(
+                        HttpStatusCode::is4xxClientError,
+                        (request, response) -> Optional.empty()
+                )
+                .requestInterceptor(
+                        new LoadBalancerInterceptor(loadBalancerClient)
+                );
+
+        // optional tracing (same pattern as ProductServiceConfig)
+        if (tracer != null && propagator != null) {
+            builder.requestInterceptor(
+                    createTracingInterceptor()
+            );
+        }
+
+        RestClient restClient = builder.build();
+
+        RestClientAdapter adapter =
+                RestClientAdapter.create(restClient);
+
+        HttpServiceProxyFactory factory =
+                HttpServiceProxyFactory.builderFor(adapter)
+                        .build();
+
         return factory.createClient(UserServiceClient.class);
+    }
+
+    private ClientHttpRequestInterceptor createTracingInterceptor() {
+
+        return (request, body, execution) -> {
+
+            if (tracer.currentSpan() != null) {
+
+                propagator.inject(
+                        tracer.currentSpan().context(),
+                        request.getHeaders(),
+                        HttpHeaders::add
+                );
+            }
+
+            return execution.execute(request, body);
+        };
     }
 
 }
